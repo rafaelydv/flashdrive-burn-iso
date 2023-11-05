48ac399977a4257410ad08c199c65ee6d12860f9
Rafael Yoshioka De Vidis <rafael.ydv@gmail.com>
2023-11-04 20:20:32 -0400

    Último update.

--- flashdrive-burn.sh
+++ flashdrive-burn.sh
@@ -12,4 +12,5 @@ read -p "Informe o caminho da imagem ISO que será gravada: " isoimage
 echo "${isoimage:?Atenção este campo é de preenchimento obrigatório\!}"
 echo
 #
+#
 dd if=$isoimage of=$flashdrive status="progress" conv="fsync"
